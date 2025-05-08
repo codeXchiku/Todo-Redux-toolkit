d1367bfd35ea2ddda21eef803a4a988c27c17a46
chiku <codexchiku@gmail.com>
2025-05-08 08:46:19 +0530

    used redux-toolkit for add and remove functionality

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { addTodo } from '../features/todo/todoSlice'
+import { useDispatch } from 'react-redux'
+import { useState } from 'react'
+
+const AddTodo = () => {
+    const[input,setInput]  = useState("")
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        dispatch(addTodo(input))
+        setInput('')
+    }
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Add new todo" 
+        value={input}
+        onChange={(e) => setInput(e.target.value)} 
+      />
+      <button onClick={handleSubmit}>Add</button>
+    </div>
+  )
+}
+
+export default AddTodo
---
+++ src/components/Todos.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
+
+const Todos = () => {
+    const todos = useSelector((state) => state.todos)
+    console.log(todos);
+    const dispatch = useDispatch()
+
+    const handleDelete = (id)=>{
+        dispatch(removeTodo(id))
+    }
+    return (
+        <>
+        <h1>Todos</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button onClick={() => handleDelete(todo.id)}> X </button>
+            </li>
+          ))}
+        </ul>
+      </>
+    )
+}
+
+export default Todos
